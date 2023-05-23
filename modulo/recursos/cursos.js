c23a589a6258f2a680fddc74b15c774e87d996f2
ANDRÉ LUIZ CONSTANTINO <22282216@dsn127m1246730.cfp127aluno.local>
2023-05-23 16:36:53 -0300

    23/05 Atualizacao de link de imagem

--- modulo/recursos/cursos.js
+++ modulo/recursos/cursos.js
@@ -1,13 +1,13 @@
 var cursos = [{
         "nome": "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla": "DS",
-        "icone": "DS.png",
+        "icone": "https://cdn.icon-icons.com/icons2/936/PNG/512/code_icon-icons.com_73620.png",
         "carga": "1200",
     },
     {
         "nome": "002 - Técnico em Redes de Computadores",
         "sigla": "RDS",
-        "icone": "redes.png",
+        "icone": "https://cdn.icon-icons.com/icons2/1812/PNG/512/4213462-communication-global-internet-network-web-worldwide-www_115426.png",
         "carga": "1200"
     }
 ];
